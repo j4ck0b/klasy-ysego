caa80aa54acb107e0638fc4d9785d921287c7396
j4ck0b <75251166+j4ck0b@users.noreply.github.com>
2023-01-15 21:05:21 +0100

    Add files via upload

---
+++ lysol.py
@@ -0,0 +1,395 @@
+#Klasa 1: Pracownik
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+
+# Klasa 2: AndrewTate
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+
+# Klasa 3: Programista
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+
+# Klasa 4: Informatyk
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+
+# Klasa 5: Kierownik
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+
+# Klasa 6: Menadżer
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+
+# Klasa 7: Dyrektor
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+
+# Klasa 8: KierownikProjektu
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+
+
+# Klasa 9: MenedżerProduktu
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+
+# Klasa 10: SpecjalistaOdMarketingu
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+
+# Klasa 11: AdministratorSieci
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+# 20. KonfiguracjaSprzętuSieciowego()
+# 21. MonitorowanieSieci()
+
+# Klasa 12: SpecjalistaDziałuIT
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+# 20. KonfiguracjaSprzętuSieciowego()
+# 21. MonitorowanieSieci()
+# 22. DiagnozowanieProblemów()
+# 23. UtrzymanieBezpieczeństwa()
+
+# Klasa 13: GrafikKomputerowy
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+# 20. KonfiguracjaSprzętuSieciowego()
+# 21. MonitorowanieSieci()
+# 22. DiagnozowanieProblemów()
+# 23. UtrzymanieBezpieczeństwa()
+# 24. TworzenieGrafiki()
+# 25. EdycjaGrafiki()
+
+# Klasa 14: SpecjalistaOdSocialMedia
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+# 20. KonfiguracjaSprzętuSieciowego()
+# 21. MonitorowanieSieci()
+# 22. DiagnozowanieProblemów()
+# 23. UtrzymanieBezpieczeństwa()
+# 24. TworzenieGrafiki()
+# 25. EdycjaGrafiki()
+# 26. TworzenieTreści()
+# 27. ZarządzanieKontamiSocialMedia()
+
+# Klasa 15: AndrewTate
+
+# Metody:
+
+# 1. PrzyjmijPracownika()
+# 2. WyślijWiadomość()
+# 3. ZorganizujSpotkanie()
+# 4. AnalizujDane()
+# 5. TworzenieKodu()
+# 6. TestowanieKodu()
+# 7. InstalacjaOprogramowania()
+# 8. UtrzymanieSprzętu()
+# 9. ZarządzanieZasobami()
+# 10. NadzorowaniePracowników()
+# 11. TworzenieBudżetu()
+# 12. PlanowanieProjektów()
+# 13. NegocjacjeKontraktowe()
+# 14. NadzorowanieProcesów()
+# 15. ZarządzanieZespołem()
+# 16. TworzenieStrategii()
+# 17. RaportowanieZmian()
+# 18. AnalizaKonkurencji()
+# 19. TworzenieKampanii()
+# 20. KonfiguracjaSprzętuSieciowego()
+# 21. MonitorowanieSieci()
+# 22. DiagnozowanieProblemów()
+# 23. UtrzymanieBezpieczeństwa()
+# 24. TworzenieGrafiki()
+# 25. EdycjaGrafiki()
+# 26. TworzenieTreści()
+# 27. ZarządzanieKontamiSocialMedia()
+# 28. AnalizaTrendów()
+# 29. OcenaEfektywności()
+
+class Pracownik:
+    def przyjmijPracownika(self):
+        pass
+    def wyślijWiadomość(self):
+        pass
+
+class AndrewTate(Pracownik):
+    def zorganizujSpotkanie(self):
+        pass
+    def analizujDane(self):
+        pass
+    
+class Programista(AndrewTate):
+    def tworzenieKodu(self):
+        pass
+    def testowanieKodu(self):
+        pass
+    
+class Informatyk(Programista):
+    def instalacjaOprogramowania(self):
+        pass
+    def utrzymanieSprzętu(self):
+        pass
+    
+class Kierownik(Informatyk):
+    def zarządzanieZasobami(self):
+        pass
+    def nadzorowaniePracowników(self):
+        pass
+    
+class Menadżer(Kierownik):
+    def tworzenieBudżetu(self):
+        pass
+    def planowanieProjektów(self):
+        pass
+    
+class Dyrektor(Menadżer):
+    def negocjacjeKontraktowe(self):
+        pass
+    def nadzorowanieProcesów(self):
+        pass
+    
+class KierownikProjektu(Dyrektor):
+    def zarządzanieZespołem(self):
+        pass
+    
+class MenedżerProduktu(KierownikProjektu):
+    def tworzenieStrategii(self):
+        pass
+    def raportowanieZmian(self):
+        pass
+    
+class SpecjalistaOdMarketingu(MenedżerProduktu):
+    def analizaKonkurencji(self):
+        pass
+    def tworzenieKampanii(self):
+        pass
+    
+class AdministratorSieci(SpecjalistaOdMarketingu):
+    def konfiguracjaSystemu(self):
+        pass
+    def monitorowanieBezpieczeństwa(self):
+        pass
+    
+class InżynierSystemowy(AdministratorSieci):
+    def automatyzacjaProcesów(self):
+        pass
+    def tworzenieSkryptów(self):
+        pass
+    
+class InżynierDanych(InżynierSystemowy):
+    def analizaDanych(self):
+        pass
+    def optymalizacjaBazyDanych(self):
+        pass
+    
+class InżynierAplikacji(InżynierDanych):
+    def tworzenieAplikacji(self):
+        pass
+    def integracjaSystemów(self):
+        pass
+    
+class Analityk(InżynierAplikacji):
+    def analizaZapotrzebowania(self):
+        pass
+    def tworzenieRaportów(self):
+        pass
